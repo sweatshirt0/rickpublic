87477a394b1b3b9c1dc558c08d059deb7d38ecda
sweatshirt0 <sweatshirttt@proton.me>
2024-03-27 12:48:05 -0400

    initial commit

---
+++ src/main/java/com/rickpublic/rickpublic/RickPublicApplication.java
@@ -0,0 +1,13 @@
+package com.rickpublic.rickpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickPublicApplication.class, args);
+	}
+
+}
